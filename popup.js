4a1c628f367a51c6c1c8cb9ee57edb9f7c7ea30a
GitPit <rifflifter@gmail.com>
2024-02-21 12:39:52 -0500

    changed .selected parameters

--- popup.js
+++ popup.js
@@ -1,4 +1,7 @@
 const sel = document.querySelector('#sel');
 const display = document.querySelector('.example');
+const toHide = document.querySelector('.columns');
 
 sel.onclick = () => display.classList.toggle("selected");
+
+window.onclick = () => toHide.style.display = 'none';
